cdb9d3ce3295929e82830a6e3e76527963e337f3
alankaiko <alamdfs@hotmail.com>
2023-11-28 01:42:24 -0300

    subindo camada de seguranca por token

--- src/main/java/br/com/scalemax/ScalemaxApiApplication.java
+++ src/main/java/br/com/scalemax/ScalemaxApiApplication.java
@@ -1,9 +1,12 @@
 package br.com.scalemax;
 
+import br.com.scalemax.config.property.ScalemaxApiProp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ScalemaxApiProp.class)
 public class ScalemaxApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/scalemax/controller/TokenResource.java
@@ -0,0 +1,35 @@
+package br.com.scalemax.controller;
+
+import br.com.scalemax.config.property.ScalemaxApiProp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Profile("oauth-security")
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+	
+	@Autowired
+	private ScalemaxApiProp scalemaxApiProp;
+
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(scalemaxApiProp.getSeguranca().isEnableHttps());
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		resp.addCookie(cookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+	
+}
