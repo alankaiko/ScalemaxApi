49c9bc299d43f043f176ee7cae6faa6e32db8bbe
alankaiko <alamdfs@hotmail.com>
2023-11-28 15:03:10 -0300

    tirando camada de seguranca pra fazer logo

--- src/main/java/br/com/scalemax/ScalemaxApiApplication.java
+++ src/main/java/br/com/scalemax/ScalemaxApiApplication.java
@@ -1,17 +1,14 @@
 package br.com.scalemax;
 
-import br.com.scalemax.config.property.ScalemaxApiProp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ScalemaxApiProp.class)
 public class ScalemaxApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScalemaxApiApplication.class, args);
-		System.out.println("APLICAÇÃO FUNCIONANDO...");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScalemaxApiApplication.class, args);
+        System.out.println("APLICAÇÃO FUNCIONANDO...");
+    }
 
 }
