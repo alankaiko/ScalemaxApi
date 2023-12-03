44e599ab3498a71216c920c1ec9e1d3d440fb9ab
alankaiko <alamdfs@hotmail.com>
2023-12-03 17:06:14 -0300

    subindo como jar e nao war

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest AS build
 
 RUN apt-get update
-RUN apt-get install openjdk-17-jdk-y
+RUN apt-get install openjdk-11-jdk
 COPY . .
 
 RUN apt-get install maven -y
@@ -14,8 +14,10 @@ COPY pom.xml /workspace
 COPY src /workspace/src
 
 RUN mvn -f pom.xml clean package
+
 FROM openjdk:11-jdk
-COPY --from=build /workspace/*.jar app.jar
+COPY --from=build /workspace/target/*.jar app.jar
+
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","app.jar"]
 
--- src/main/java/br/com/scalemax/ScalemaxApiApplication.java
+++ src/main/java/br/com/scalemax/ScalemaxApiApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScalemaxApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ScalemaxApiApplication.class, args);
-        System.out.println("APLICAÇÃO FUNCIONANDO...");
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ScalemaxApiApplication.class, args);
+		System.out.println("APLICAÇÃO FUNCIONANDO...");
+	}
 
 }
